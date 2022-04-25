b62c07222071756a1bf676a9077c24e637f45642
Sara Salma <sarasalma200@gmail.com>
2022-04-26 01:01:02 +0200

    Standard Library

---
+++ LIB/DEFINE.h
@@ -0,0 +1,38 @@
+/*
+ * DEFINE.h
+ *
+ *  Created on: Apr 12, 2022
+ *      Author: raya
+ */
+
+#ifndef LIB_DEFINE_H_
+#define LIB_DEFINE_H_
+
+#define ZERO          0
+#define ONE           1
+#define TWO           2
+#define THREE         3
+#define FOUR          4
+#define FIVE          5
+#define SIX           6
+#define SEVEN         7
+#define EIGHT         8
+#define NINE          9
+#define TEN           10
+#define ONE_HUNDRED   100
+
+
+
+
+#define OUTPUT 0xff
+
+#define OK  0
+#define NOK 1
+
+#define WAIT 1000
+
+#define TIME1 15
+#define TIME2 10
+#define TIME3 5
+
+#endif /* LIB_DEFINE_H_ */
